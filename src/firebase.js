1dd84b5f223a04b0dfb4ea7b01cda4fcd6574948
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2025-01-18 23:00:30 -0600

    Se agrego firestore y hosting de firebase con exito, ya se puede acceder a la pagina web

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxw_HXTXtaKn5XnARrS4E57Wsg7IuSHf0",
+  authDomain: "constanciaskc.firebaseapp.com",
+  projectId: "constanciaskc",
+  storageBucket: "constanciaskc.firebasestorage.app",
+  messagingSenderId: "32493179664",
+  appId: "1:32493179664:web:a97f6d3aca5a29bcec4b95"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const dbkc = getFirestore(app);
